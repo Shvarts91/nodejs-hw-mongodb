a6fc7566900610d987c457efd614e3676de08be1
Artem Shvachov <shvachev91@gmail.com>
2025-07-30 21:44:51 +0300

    update server.js file

--- src/server.js
+++ src/server.js
@@ -47,9 +47,7 @@ export const setupServer = () => {
     res.status(200).json({
       status: 200,
       message: `Successfully found contact with id ${contactId}!`,
-      data: {
-        contact,
-      },
+      data: contact,
     });
   });
 
