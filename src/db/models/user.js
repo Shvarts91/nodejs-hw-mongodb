7d1c8100570c0510529984ef231c57d708f98635
Artem Shvachov <shvachev91@gmail.com>
2025-08-17 19:40:32 +0300

    hide password

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -16,7 +16,16 @@ const usersSchema = new Schema(
       required: true,
     },
   },
-  { timestamps: true },
+  {
+    timestamps: true,
+    toJSON: {
+      transform(doc, ret) {
+        delete ret.password;
+        delete ret.__v;
+        return ret;
+      },
+    },
+  },
 );
 
 export const UsersCollection = model('users', usersSchema);
