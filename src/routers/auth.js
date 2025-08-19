3cd710c0c58b5c75fa56ace198f5eb0486bfde13
Artem Shvachov <shvachev91@gmail.com>
2025-08-19 21:04:28 +0300

    first commit hw-6

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,12 +1,17 @@
 import { Router } from 'express';
 import { validateContact } from '../middlewares/validateContact.js';
-import { loginUserSchema, registerUserSchema } from '../validation/auth.js';
+import {
+  loginUserSchema,
+  registerUserSchema,
+  requestResetEmailSchema,
+} from '../validation/auth.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import {
   loginUserController,
   logoutUserController,
   refreshUserSessionController,
   registerUserController,
+  requestResetEmailController,
 } from '../controllers/auth.js';
 
 const router = Router();
@@ -26,5 +31,11 @@ router.post(
 router.post('/auth/refresh', ctrlWrapper(refreshUserSessionController));
 
 router.post('/auth/logout', ctrlWrapper(logoutUserController));
+
+router.post(
+  '/auth/send-reset-email',
+  validateContact(requestResetEmailSchema),
+  ctrlWrapper(requestResetEmailController),
+);
 
 export default router;
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,9 +1,12 @@
 import bcrypt from 'bcrypt';
 import { randomBytes } from 'crypto';
-import { UsersCollection } from '../db/models/user.js';
 import createHttpError from 'http-errors';
+import jwt from 'jsonwebtoken';
 import { SessionsCollection } from '../db/models/session.js';
-import { FIFTEEN_MINUTES, ONE_DAY } from '../constants/index.js';
+import { FIFTEEN_MINUTES, ONE_DAY, SMTP } from '../constants/index.js';
+import { UsersCollection } from '../db/models/User.js';
+import { getEnvVar } from '../utils/getEnvVar.js';
+import { sendEmail } from '../utils/sendMail.js';
 
 export const registerUser = async (payload) => {
   const user = await UsersCollection.findOne({ email: payload.email });
@@ -82,4 +85,28 @@ export const refreshUsersSession = async ({ sessionId, refreshToken }) => {
 
 export const logoutUser = async (sessionId) => {
   await SessionsCollection.deleteOne({ _id: sessionId });
+};
+
+export const requestResetToken = async (email) => {
+  const user = await UsersCollection.findOne({ email });
+  if (!user) {
+    throw createHttpError(404, 'User not found');
+  }
+  const resetToken = jwt.sign(
+    {
+      sub: user._id,
+      email,
+    },
+    getEnvVar('JWT_SECRET'),
+    {
+      expiresIn: '15m',
+    },
+  );
+
+  await sendEmail({
+    from: getEnvVar(SMTP.SMTP_FROM),
+    to: email,
+    subject: 'Reset your password',
+    html: `<p>Click <a href="${resetToken}">here</a> to reset your password!</p>`,
+  });
 };
